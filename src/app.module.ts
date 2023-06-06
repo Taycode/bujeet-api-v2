9d192ef0ee317c5014c342b48350ed47c4e1aa68
Abdulmateen Tairu <tay2druh@gmail.com>
2023-06-06 07:03:03 +0100

    fix: fixed DB url

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,20 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { BankModule } from './bank/bank.module';
-import { BudgetModule } from "./budget/budget.module";
-import { BullModule } from "@nestjs/bull";
+import { BudgetModule } from './budget/budget.module';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/bujit', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGODB_URL,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }),
     }),
     UserModule,
     BankModule,
